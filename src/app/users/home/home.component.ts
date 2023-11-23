88f2581bcfad7cc3172905131495d041be614d37
viswabalamurugan2002 <viswabalamurugan2002@gmail.com>
2023-11-23 12:40:43 +0530

    home

--- src/app/users/home/home.component.ts
+++ src/app/users/home/home.component.ts
@@ -1,30 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent {//implements OnInit
+  users : any[] = [];
+  apiUrl : string = 'https://retoolapi.dev/8jwHfY/users';
 
-  constructor(private router : Router){
+  constructor(private router : Router,private apiSevice:ApiService){
     console.log("Constructor is called....");
   }
   
-  ngOnInit(): void {
-    console.log("NgOnInit is called.....");
-    this.checkSession();
-  }
+  // ngOnInit(): void {
+  //   console.log("NgOnInit is called.....");
+  //   this.checkSession();
+  // }
 
 
   checkSession(){
-    if(sessionStorage.getItem('email') == null){
-      this.router.navigate(['/login']);
-    }
-  }
+    let url = this.apiUrl;
+      url += "?email=" + sessionStorage.getItem('email')
+      this.apiSevice.getData(url).subscribe(
+        (data : any) => {
+          this.users = data;
+        
+          
 
-  redirectToLogout(){
-    this.router.navigate(['/logout']);
+          for(let user of this.users){
+            if(user.email != sessionStorage.getItem('email') ){
+              this.router.navigate(['/login'])
+              
+            
+             
+             
+            }
+            
+           
+            }
+
+
+    
+  })
+  if(sessionStorage.getItem('email')==null){
+    this.router.navigate(['/login'])
   }
+  if(this.users.length==0){
+    this.router.navigate(['/login'])
+  }
+}
+
+ 
 }
